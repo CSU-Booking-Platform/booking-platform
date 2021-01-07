1b8abe00086dd4657e452abc7def31ccc2c46e0b
jonathanp16 <perezjonathan301@gmail.com>
2021-01-06 23:40:59 -0500

    feat(#214) Refactored Table to RoomTable, added demo page to only work in dev mode
    The tables will be switched from being dynamic to now being seperated into one table component per table needed (ex RoomTable, BookingRequestTable etc..) since it will be much easier to work with table actions. Also added a demo route for the table that will only be accessible in a local dev environment and not in production

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -14,39 +14,21 @@
                 </div>
             </div>
         </div>
-
-        <div class="py-12">
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <Table 
-                :tableHeaders="tableHeaders"
-                :tableCaption="tableCaption"
-                :tableData="tableData"
-                />
-            </div>
-        </div>
     </app-layout>
 </template>
 
 <script>
     import AppLayout from '@src/Layouts/AppLayout'
     import Welcome from '@src/Jetstream/Welcome'
-    import Table from '@src/Components/Tables/Table'
 
     export default {
         components: {
             AppLayout,
             Welcome,
-            Table
         },
           data() {
             return {
-                tableHeaders: ['th1', 'th2', 'th3', 'th4', 'th5'],
-                tableCaption: '',
-                tableData: [
-                    ['tr1', 'Tr1', 'tr1', 'tr1', 'tr1'],
-                    ['tr2', 'tr2', 'tr2', 'tr2', 'tr2'],
-                    ['tr3', 'tr3', 'tr3', 'tr3', 'tr3']
-                ]
+
             }
   }
     }

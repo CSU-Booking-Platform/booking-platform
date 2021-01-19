768732f476f65e7ecd6ab1928dcd72eb06affe75
keegan <woods>
2021-01-19 14:36:25 -0500

    feat(#253): Download booking request reference files

--- resources/js/Pages/Admin/BookingRequests/BookingRequestsList.vue
+++ resources/js/Pages/Admin/BookingRequests/BookingRequestsList.vue
@@ -10,35 +10,48 @@
         <!-- Rooms List -->
         <template #content>
           <div class="space-y-6">
-            <div class="grid grid-cols-7">
+            <div class="grid grid-cols-8">
               <div class="text-md mx-3">User</div>
               <div class="text-md mx-3">Room</div>
               <div class="text-md mx-3">Start Time</div>
               <div class="text-md mx-3">End time</div>
               <div class="text-md mx-3">Status</div>
+              <div class="text-md mx-3">Reference</div>
             </div>
 
             <div
-              v-for="booking_request in booking_requests"
-              :key="booking_request.id"
+              v-for="bookingRequest in bookingRequests"
+              :key="bookingRequest.id"
               class="grid flex items-center"
             >
-              <div class="grid grid-cols-7">
-                <div class="text-md mx-3">{{ booking_request.user.name }}</div>
-                <div class="text-md mx-3">{{ booking_request.room.name }}</div>
-                <div class="text-md mx-3">{{ calendar(booking_request.start_time) }}</div>
-                <div class="text-md mx-3">{{ calendar(booking_request.end_time) }}</div>
-                <div class="text-md mx-3">{{ booking_request.status }}</div>
+              <div class="grid grid-cols-8">
+                <div class="text-md mx-3">{{ bookingRequest.user.name }}</div>
+                <div class="text-md mx-3">{{ bookingRequest.room.name }}</div>
+                <div class="text-md mx-3">{{ calendar(bookingRequest.start_time) }}</div>
+                <div class="text-md mx-3">{{ calendar(bookingRequest.end_time) }}</div>
+                <div class="text-md mx-3">{{ bookingRequest.status }}</div>
+                <div class="text-md mx-3">
+                  <a 
+                    v-if="bookingRequest.reference.path"
+                    @click="setReference(bookingRequest);" 
+                    class="cursor-pointer text-sm text-blue-800 focus:outline-none" 
+                    :href="href"
+                  >Download</a>
+                  <a 
+                    v-else
+                    class="text-sm focus:outline-none" 
+                  >No Files Submitted</a>
+                </div>
                 <div class="text-md mx-3">
                   <button
                     class="cursor-pointer ml-6 text-sm text-blue-800 focus:outline-none"
-                    @click="bookingRequestBeingUpdated = booking_request"
+                    @click="bookingRequestBeingUpdated = bookingRequest"
                   >Update</button>
                 </div>
                 <div class="text-md mx-3">
                   <button
                     class="cursor-pointer ml-6 text-sm text-blue-800 focus:outline-none"
-                    @click="bookingRequestBeingDeleted = booking_request"
+                    @click="bookingRequestBeingDeleted = bookingRequest"
                   >Delete</button>
                 </div>
               </div>
@@ -46,7 +59,7 @@
           </div>
 
           <UpdateBookingRequestForm
-            :booking_request="bookingRequestBeingUpdated"
+            :bookingRequest="bookingRequestBeingUpdated"
             :availableRooms="availableRooms"
             @close="bookingRequestBeingUpdated = null"
           ></UpdateBookingRequestForm>
@@ -91,10 +104,11 @@ import JetInputError from "@src/Jetstream/InputError";
 import JetLabel from "@src/Jetstream/Label";
 import UpdateBookingRequestForm from "./UpdateBookingRequestForm";
 import Label from "@src/Jetstream/Label";
+const moment= require('moment') 
 
 export default {
   props: {
-    booking_requests: {
+    bookingRequests: {
       type: Array,
       default: function() {
         return [];
@@ -128,8 +142,10 @@ export default {
   data() {
     return {
       deleteBookingRequestForm: this.$inertia.form(),
+      bookingReference: '',
       bookingRequestBeingUpdated: null,
-      bookingRequestBeingDeleted: null
+      bookingRequestBeingDeleted: null,
+      
     };
   },
 
@@ -145,12 +161,21 @@ export default {
         });
     },
     calendar(timestamp) {
-        return moment(timestamp).local().format('LLL');
+      return moment(timestamp).local().format('LLL');
     },
+    setReference(e) {
+      this.bookingReference = e.reference.path;
+    }
   },
   computed: {
         availableRooms: function () {
             return this.rooms.filter(room => room.status == "available");
+        },
+        href () {
+          if(this.bookingReference)
+          {
+            return 'bookings/download/' + this.bookingReference;
+          }          
         }
     }
 };

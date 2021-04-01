e884850ed01c1bdf7cbcf4653debcd42e4c632fb
keegan <woods>
2021-04-01 17:36:02 -0400

    fix(#530): improved method and variable names

--- resources/js/Pages/Admin/BookingRequests/CreateBookingRequestModal.vue
+++ resources/js/Pages/Admin/BookingRequests/CreateBookingRequestModal.vue
@@ -182,7 +182,7 @@ export default {
       this.createBookingRequestForm.reservations.splice(pos,1)
     },
     createBookingRequest() {
-      this.setCreate(true);
+      this.setLocalIsCreating(true);
       this.createBookingRequestForm.post("/bookings/create", {
         preserveScroll: true
       }).then(() => {
@@ -190,7 +190,7 @@ export default {
           this.closeModal();
         }
         else{
-          this.setCreate(false);
+          this.setLocalIsCreating(false);
         }
       });
     },
@@ -203,8 +203,8 @@ export default {
         this.createBookingRequestForm.reference.push(file);
       }
     },
-    setCreate(val) {
-      localStorage.create = val;
+    setLocalIsCreating(val) {
+      localStorage.isCreatingBooking = val;
     },
     setDuration()
     {
